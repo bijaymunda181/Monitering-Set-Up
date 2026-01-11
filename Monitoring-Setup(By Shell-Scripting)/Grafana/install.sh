9d78c8d05d1c84e10775f84a6fc33d4c35a75bb5
Bijay <bijay78082@gmail.com>
2026-01-11 17:42:00 +0530

    Node_exporter

--- Monitoring-Setup(By Shell-Scripting)/Grafana/install.sh
+++ Monitoring-Setup(By Shell-Scripting)/Grafana/install.sh
@@ -5,11 +5,11 @@ echo -e "\e[32mConfiguring Repo\e[0m"
 cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
 [grafana]
 name=grafana
-baseurl=https://rpm.grafana.com
+baseurl=https://packages.grafana.com/oss/rpm
 repo_gpgcheck=1
 enabled=1
 gpgcheck=1
-gpgkey=https://rpm.grafana.com/gpg.key
+gpgkey=https://packages.grafana.com/gpg.key
 sslverify=1
 sslcacert=/etc/pki/tls/certs/ca-bundle.crt
 EOF
