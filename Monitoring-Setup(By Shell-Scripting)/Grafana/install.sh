ce1005116fe0da18a03179414a0e553989073560
Bijay <bijay78082@gmail.com>
2026-01-11 17:32:12 +0530

    Node_exporter

---
+++ Monitoring-Setup(By Shell-Scripting)/Grafana/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+git pull &> /tmp/grafana.log
+
+echo -e "\e[32mConfiguring Repo\e[0m"
+cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://rpm.grafana.com
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://rpm.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+echo -e "\e[32mInstalling Grafana\e[0m"
+yum install grafana -y &>> /tmp/grafana.log
+
+
+echo -e "\e[32m starting grafana \e[0m"
+systemctl enable grafana &>> /tmp/grafana.log
+systemctl restart grafana &>> /tmp/grafana.log
+
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mService Started file\e[0m"
+else
+  echo -e "\e[31mFailed to start the Service\e[0m"
+fi
+
